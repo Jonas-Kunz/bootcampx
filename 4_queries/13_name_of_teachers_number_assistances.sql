c621c0bf3d8f729d5fb829642e0ebc1cda0b5e4b
Jonas-Kunz <jonas_K_7@outlook.com>
2024-02-06 15:26:21 -0400

    completed bootcampx

---
+++ 4_queries/13_name_of_teachers_number_assistances.sql
@@ -0,0 +1,13 @@
+SELECT 
+  DISTINCT teachers.name AS teacher, 
+  cohorts.name AS cohort, 
+  count(assistance_requests.teacher_id) AS total_assistances
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = 'JUL02'
+GROUP BY 
+  teachers.name,
+  cohorts.name
+ORDER BY teachers.name;
